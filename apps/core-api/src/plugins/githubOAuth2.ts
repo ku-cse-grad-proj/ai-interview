f3a5334b2cea027180f78615b87795d3599c9bfd
나형진 <skgudwls@konkuk.ac.kr>
2025-07-16 12:07:30 +0900

    feat: 콜백 처리 및 JWT 발급

--- apps/core-api/src/plugins/githubOAuth2.ts
+++ apps/core-api/src/plugins/githubOAuth2.ts
@@ -1,4 +1,5 @@
 import oauthPlugin, { OAuth2Namespace } from '@fastify/oauth2'
+import axios from 'axios'
 import { FastifyPluginAsync } from 'fastify'
 import fp from 'fastify-plugin'
 
@@ -6,6 +7,20 @@ declare module 'fastify' {
   interface FastifyInstance {
     githubOAuth2: OAuth2Namespace
   }
+}
+
+interface GitHubUserInfo {
+  id: number
+  login: string
+  name: string
+  email: string | null
+}
+
+interface GitHubEmailInfo {
+  email: string
+  primary: boolean
+  verified: boolean
+  visibility: 'public' | 'private' | null
 }
 
 const githubOAuth2Plugin: FastifyPluginAsync = async (fastify) => {
@@ -28,8 +43,49 @@ const githubOAuth2Plugin: FastifyPluginAsync = async (fastify) => {
       const { token } =
         await fastify.githubOAuth2.getAccessTokenFromAuthorizationCodeFlow(req)
 
-      // TODO: 다음 태스크에서 사용자 정보 조회 및 JWT 발급 예정
-      reply.send({ accessToken: token.access_token })
+      const { data: userInfo } = await axios.get<GitHubUserInfo>(
+        'https://api.github.com/user',
+        {
+          headers: { Authorization: `Bearer ${token.access_token}` },
+        },
+      )
+
+      let userEmail = userInfo.email
+      // 사용자의 주 이메일이 비공개인 경우, 이메일 목록에서 가져옵니다.
+      if (!userEmail) {
+        const { data: emails } = await axios.get<GitHubEmailInfo[]>(
+          'https://api.github.com/user/emails',
+          {
+            headers: { Authorization: `Bearer ${token.access_token}` },
+          },
+        )
+        userEmail =
+          emails.find((email) => email.primary && email.verified)?.email ?? null
+      }
+
+      if (!userEmail) {
+        throw new Error('GitHub user email could not be retrieved.')
+      }
+
+      // DB에서 사용자 조회 또는 생성
+      let user = await fastify.prisma.user.findUnique({
+        where: { email: userEmail },
+      })
+
+      if (!user) {
+        user = await fastify.prisma.user.create({
+          data: {
+            email: userEmail,
+            name: userInfo.name || userInfo.login,
+            provider: 'GITHUB',
+          },
+        })
+      }
+
+      // 사용자를 위한 JWT 생성
+      const serviceToken = fastify.jwt.sign({ userId: user.id })
+
+      reply.send({ serviceToken })
     } catch (error) {
       fastify.log.error(error)
       reply.status(500).send({ message: 'GitHub login failed' })
